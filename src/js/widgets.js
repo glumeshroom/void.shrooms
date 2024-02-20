1da2fc565146e2328c2bc58bc3a882541b66333d
Rodrick <other@deadinsideartist.art>
2024-02-19 19:50:08 -0600

    Fixing generation issue

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -31,8 +31,7 @@ function generate2() {
         hexT = "33FF00";
     }
 
-    let text = `<div id="voidShrooms"><style>#voidShrooms{width:200px;height:50px;background:url("https://void.shroom.ink/src/img/background.webp") #171717;font-family:monospace;outline:5px double #${hexB};outline-offset:-5px;display:grid;grid-template-columns:1fr 1fr 1fr;margin:.5em}#voidShrooms a{color:#efefef;text-decoration:none;font-size:2.2em;text-align:center;align-self:center}#voidShrooms #home{font-size:1em;color:#${hexT}}</style><a aria-label="Previous" href="https://void.shroom.ink/${slug}/previous" id="previous" target="_blank" title="previous">←</a> <a aria-label="Home" href="https://void.shroom.ink/" id=
-	home" target="_blank" title="home">void.shrooms</a> <a aria-label="Next" href="https://void.shroom.ink/${slug}/next" id="next" target="_blank" title="next">→</a></div>`;
+    let text = `<div id="voidShrooms"><style>#voidShrooms{width:200px;height:50px;background:url("https://void.shroom.ink/src/img/background.webp") #171717;font-family:monospace;outline:5px double #${hexB};outline-offset:-5px;display:grid;grid-template-columns:1fr 1fr 1fr;margin:.5em}#voidShrooms a{color:#efefef;text-decoration:none;font-size:2.2em;text-align:center;align-self:center}#voidShrooms #home{font-size:1em;color:#${hexT}}</style><a aria-label="Previous" href="https://void.shroom.ink/${slug}/previous" id="previous" target="_blank" title="previous">←</a> <a aria-label="Home" href="https://void.shroom.ink/" id="home" target="_blank" title="home">void.shrooms</a> <a aria-label="Next" href="https://void.shroom.ink/${slug}/next" id="next" target="_blank" title="next">→</a></div>`;
 
     preview2.innerHTML = text;
     code2.innerHTML = text;
